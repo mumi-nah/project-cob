10fb157cbbd35dba84d773b5232bf1a5f4734238
Muminah <muminahabdulkadir@gmail.com>
2026-08-29 21:24:01 +0100

    network module

--- modules/vpc_network/main.tf
+++ modules/vpc_network/main.tf
@@ -1,23 +1,63 @@
+resource "aws_vpc" "vpc" {
+  cidr_block             = var.vpc-cidr
+  enable_dns_hostnames   = true
+  enable_dns_support     = true
+  instance_tenancy = "default"
 
-
-
-
-# vpc block
-module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-
-  name = "cob-vpc"
-  cidr = "10.0.0.0/16"
-
-  azs             = ["eu-west-1a", "eu-west-1b"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-  enable_nat_gateway = true
-  enable_vpn_gateway = true
-
-  tags = {
-    Terraform = "true"
-    Environment = "dev"
-  }
+  tags = merge(local.common_tags, {
+    Name = '${local.name_prefix}-vpc'
+  })
 }
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = merge(local.common_tags, {
+    Name = '${local.name_prefix}-igw'
+  })
+}
+
+resource "aws_subnet" "public" {
+  count                       = length(var.public-subnet-cidrs)
+  vpc_id                      = aws_vpc.vpc.id
+  cidr_block                  = var.public-subnet-cidrs[count.index]
+  availability_zones          = var.availability-zones[count.index]
+  map_to_public_ip_on_launch  = true
+
+  tags = merge(local.common_tags, {
+    Name = "${local.name_prefix}-public-${var.availability_zones[count.index]}"
+    Tier = "public"
+  })
+}
+
+resource "aws_subnet" "private" {
+  count                       = length(var.private-subnet-cidrs)
+  vpc_id                      = aws_vpc.vpc.id
+  cidr_block                  = var.private-subnet-cidrs[count.index]
+  availability_zones          = var.availability-zones[count.index]
+
+  tags = merge(local.common_tags, {
+    Name = "${local.name_prefix}-private-${var.availability_zones[count.index]}"
+    Tier = "private"
+  })
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = merge(local.common_tags, {
+    Name = "${local.name_prefix}-public-rt"
+  })
+}
+
+resource "aws_route_table_association" "public" {
+  count          = length(aws_subnet.public)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.example.id
+}
+
